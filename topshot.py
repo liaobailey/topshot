212f93f888fa9711f495efcd69041a6de0822daa
liaobailey <bailey.liao@gmail.com>
2021-02-06 13:21:22 -0500

    new python file

--- topshot.py
+++ topshot.py
@@ -4,25 +4,30 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import altair as alt
+import unidecode
 
-st.title("NBA Top Shot Data Visualization")
+st.title("NBA Top Shot Data Visualization, Updated Daily")
 
-st.subheader("Top 10 Transaction Value as of 1/15/2021")
+st.subheader("Top 10 Transaction Value as of 2/6/2021")
 
 @st.cache(persist = True)
 def load_data(csv, nrows):
     data = pd.read_csv(csv, nrows = nrows)
     return data
 
-raw = load_data('topshot_115.csv', 200000)
-new = raw.drop(columns = ['Unnamed: 0', 'Unnamed: 0.1'])
+raw = load_data('master.csv', 900000)
+new = raw.drop(columns = ['Unnamed: 0'])
 
 data = new[new['Set'].notnull()]
 data['USD'] = data['USD'].map(lambda x: x.replace('$', ''))
 data['USD'] = data['USD'].map(lambda x: x.replace(',', ''))
 data['USD'] = data['USD'].map(lambda x: float(x))
 
-data.columns = ['Month', 'Player', 'Set', 'Team', 'Category', 'Price', 'USD', 'Serial', 'Seller', 'Buyer']
+data.columns = ['Sold', 'Player', 'Set', 'Team', 'Category', 'Price', 'USD', 'Serial', 'Seller', 'Buyer', 'Month']
+
+data['Player'] = data['Player'].map(lambda x: x.replace("\xa0", " "))
+data['Player'] = data['Player'].map(lambda x: unidecode.unidecode(x))
+
 
 display_1 = data.sort_values('USD', ascending = False)[['Player', 'Set','Team','Category','Price']].head(10).set_index('Player')
 
@@ -66,7 +71,7 @@ group = data.groupby(['Month', 'Category']).agg({
 }).reset_index()
 
 st.write(alt.Chart(group).mark_line().encode(
-    x=alt.X('Month', sort=['Current Month', '1 month ago', '2 months ago', '3 months ago', '4 months ago', '5 months ago']),
+    x=alt.X('Month', sort = ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May','Jun']),
     y='USD',
     color='Category',
     strokeDash='Category'
@@ -82,7 +87,7 @@ group_2 = data.groupby(['Month', 'Category']).agg({
 }).reset_index()
 
 st.write(alt.Chart(group_2).mark_line().encode(
-    x=alt.X('Month', sort=['Current Month', '1 month ago', '2 months ago', '3 months ago', '4 months ago', '5 months ago']),
+    x=alt.X('Month', sort = ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May','Jun']),
     y='USD',
     color='Category',
     strokeDash='Category'
@@ -91,7 +96,7 @@ st.write(alt.Chart(group_2).mark_line().encode(
     height=600
 ))
 
-st.subheader("Price change for most traded cards - Last 100 Transactions")
+st.subheader("Price change for most traded cards - Last 500 Transactions")
 
 display_3_pre = data.groupby(['Player', 'Set', 'Team', 'Category']).agg({
     'Buyer':'count'
@@ -100,17 +105,15 @@ display_3_pre = data.groupby(['Player', 'Set', 'Team', 'Category']).agg({
 lft = display_3_pre.head(10)
 merge = pd.merge(data, lft, on = ['Player', 'Set', 'Team', 'Category'], how = 'left')
 display_3 = merge[merge['Buyer_y'].notnull()]
-display_3['Player'] = display_3['Player'].map(lambda x: x.replace("\xa0", " "))
-display_3['unique'] = display_3['Player'] + ': ' + display_3['Set']
+display_3['unique'] = display_3['Player'] + ': ' + display_3['Set'] + ' - ' + display_3['Category']
 lst = display_3['unique'].unique()
 
 option = st.selectbox('Choose Moment',(lst[0], lst[1], lst[2], lst[3], lst[4], lst[5], lst[6], lst[7], lst[8], lst[9]))
 
-d_3 = display_3[(display_3['Player'] == option.split(':')[0]) & (display_3['Set'] == option.split(': ')[1])]
-
+d_3 = display_3[(display_3['Player'] == option.split(':')[0]) & (display_3['Set'] == option.split(': ')[1].split(' -')[0]) & (display_3['Category'] == option.split('- ')[1])]
 dis_3 = d_3.reindex(index=d_3.index[::-1])
 
-displ_3 = dis_3[-100:].reset_index().reset_index().rename(columns = {'level_0': 'tranx'})
+displ_3 = dis_3[-500:].reset_index().reset_index().rename(columns = {'level_0': 'tranx'})
 displ_3['tranx'] = displ_3['tranx'] + 1
 
 st.write(alt.Chart(displ_3).mark_circle(size=60).encode(
@@ -123,7 +126,7 @@ st.write(alt.Chart(displ_3).mark_circle(size=60).encode(
     height=600
 ))
 
-st.subheader("Price change for Biggest Delta in value cards - Last 100 Transactions")
+st.subheader("Price change for Biggest Delta in value cards - Last 500 Transactions")
 
 display_4_pre = data.groupby(['Player', 'Set', 'Team', 'Category']).agg({
     'USD':['min', 'max']
@@ -136,20 +139,40 @@ rgt = display_4_pre.sort_values('diff', ascending = False).head(10)
 merge_2 = pd.merge(data, rgt, on = ['Player', 'Set', 'Team', 'Category'], how = 'left')
 
 display_4 = merge_2[merge_2['diff'].notnull()]
-display_4['Player'] = display_4['Player'].map(lambda x: x.replace("\xa0", " "))
-display_4['unique'] = display_4['Player'] + ': ' + display_4['Set']
+display_4['unique'] = display_4['Player'] + ': ' + display_4['Set'] + ' - ' + display_4['Category']
 lst_2 = display_4['unique'].unique()
 
 option_2 = st.selectbox('Choose Moment ',(lst_2[0], lst_2[1], lst_2[2], lst_2[3], lst_2[4], lst_2[5], lst_2[6], lst_2[7], lst_2[8], lst_2[9]))
 
-d_4 = display_4[(display_4['Player'] == option_2.split(':')[0]) & (display_4['Set'] == option_2.split(': ')[1])]
-
+d_4 = display_4[(display_4['Player'] == option_2.split(':')[0]) & (display_4['Set'] == option_2.split(': ')[1].split(' -')[0]) & (display_4['Category'] == option_2.split('- ')[1])]
 dis_4 = d_4.reindex(index=d_4.index[::-1])
 
-displ_4 = dis_4[-100:].reset_index().reset_index().rename(columns = {'level_0': 'tranx'})
+displ_4 = dis_4[-500:].reset_index().reset_index().rename(columns = {'level_0': 'tranx'})
 displ_4['tranx'] = displ_4['tranx'] + 1
 
 st.write(alt.Chart(displ_4).mark_circle(size=60).encode(
+    x='tranx',
+    y='USD',
+    color='Set',
+    tooltip=['Player', 'Set', 'Category', 'tranx', 'USD']
+).interactive().properties(
+    width=900,
+    height=600
+))
+
+st.subheader("Find Last 500 Tranx from any play")
+
+player = st.text_input('Input full player name:', 'Luka Doncic')
+pack = st.selectbox('Choose Pack', list(data['Set'].unique()))
+cat = st.selectbox('Choose Play', list(data['Category'].unique()))
+year = st.text_input('Choose Year (format 2020-21):', '2019-20')
+
+d_5 = data[(data['Player'].str.lower().str.contains(player.lower())) & (data['Set'] == pack) & (data['Category'] == cat) & (data['Player'].str.contains(year))]
+dis_5 = d_5.reindex(index=d_5.index[::-1])
+displ_5 = dis_5[-500:].reset_index().reset_index().rename(columns = {'level_0': 'tranx'})
+displ_5['tranx'] = displ_5['tranx'] + 1
+
+st.write(alt.Chart(displ_5).mark_circle(size=60).encode(
     x='tranx',
     y='USD',
     color='Set',
